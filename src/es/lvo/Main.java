36be08263eed87a1c41d0b0e24581e2374c98cf5
Lucía Vidal Ortiz <opalo97230704@gmail.com>
2022-02-25 13:50:11 +0100

    no acabado

--- src/es/lvo/Main.java
+++ src/es/lvo/Main.java
@@ -9,11 +9,20 @@ public class Main {
         int[] array2 = new int[5];
         Scanner entrada = new Scanner(System.in);
 
-        System.out.println("Dame 5 numeros: ");
+        System.out.println("Dame cinco números para crear un array con ellos...");
         for (int i = 0; i < array1.length; i++) {
             System.out.print("Número " + (i + 1) + ": ");
             array1[i] = entrada.nextInt();
         }
 
+        for (int i = 0; i < array1.length; i++) {
+            array2[i] = array1[i];
+        }
+        System.out.println("Array 1:" + array1);
+
+        for (int i = 0; i < array2.length; i++) {
+            array2[i] = array1[i];
+        }
+        System.out.println("Array 2:");
     }
 }
