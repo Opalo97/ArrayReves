6af7018f23c172a7cbcac27c15cb419f2a5d7c65
Lucía Vidal Ortiz <opalo97230704@gmail.com>
2022-02-28 10:26:27 +0100

    acabado

--- src/es/lvo/Main.java
+++ src/es/lvo/Main.java
@@ -9,20 +9,32 @@ public class Main {
         int[] array2 = new int[5];
         Scanner entrada = new Scanner(System.in);
 
+        // Pedimos los números y los guardamos en el array 1
         System.out.println("Dame cinco números para crear un array con ellos...");
         for (int i = 0; i < array1.length; i++) {
             System.out.print("Número " + (i + 1) + ": ");
             array1[i] = entrada.nextInt();
         }
 
+        // Guardamos los números del array 1 en el array 2, pero al revés, de atrás
+        // hacia adelante.
         for (int i = 0; i < array1.length; i++) {
-            array2[i] = array1[i];
+            array2[array2.length - i - 1] = array1[i];
         }
-        System.out.println("Array 1:" + array1);
 
-        for (int i = 0; i < array2.length; i++) {
-            array2[i] = array1[i];
+        // Mostramos los elementos del array 1.
+        System.out.println("Array 1:");
+        for (int i = 0; i < array1.length; i++) {
+            System.out.print(array1[i] + " ");
         }
+
+        // Mostramos los elementos del array 2.
+        System.out.println();
         System.out.println("Array 2:");
+        for (int i = 0; i < array2.length; i++) {
+            System.out.print(array2[i] + " ");
+        }
+
+        entrada.close();
     }
 }
